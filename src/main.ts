ed67527f5743a0d5236b802608630d47fa5239d9
HugoRomz <rosalesrafael1@hotmail.com>
2025-05-07 02:17:23 -0600

    feat: simplificar la obtención de la clave pública de Clerk en la configuración

--- src/main.ts
+++ src/main.ts
@@ -11,8 +11,7 @@ import router from './router'
 import { clerkPlugin } from '@clerk/vue'
 import { esMX } from '@clerk/localizations'
 
-const PUBLISHABLE_KEY =
-  import.meta.env.CLERK_PUBLISHABLE_KEY || import.meta.env.VITE_CLERK_PUBLISHABLE_KEY
+const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY
 
 if (!PUBLISHABLE_KEY) {
   throw new Error('Add your Clerk Publishable Key to the .env file')
